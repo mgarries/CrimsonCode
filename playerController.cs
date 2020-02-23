fe0056fce12d2022d71a8285f83f5324a279e6f2
Michael Garries <55902324+mgarries@users.noreply.github.com>
2020-02-23 02:36:49 -0800

    Added playerController

---
+++ playerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int maxHealth = 5;
+    int currentHealth;
+    
+    float playerSpeed;
+    Rigidbody2D rigidBody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = 4.0f;
+        rigidBody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = rigidBody2d.position;
+        position.x = position.x + playerSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + playerSpeed * vertical * Time.deltaTime;
+
+        rigidBody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
